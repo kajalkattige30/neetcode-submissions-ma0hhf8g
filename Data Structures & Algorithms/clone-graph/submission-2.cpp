1052d75f15285bb5ee7b5a3f331b3a6eb7b6fa64
Kajal Kattige <35621828+kajalkattige30@users.noreply.github.com>
2026-04-01 18:41:09 +0530

    Add: clone-graph - submission-2

---
+++ Data Structures & Algorithms/clone-graph/submission-2.cpp
@@ -0,0 +1,81 @@
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    vector<Node*> neighbors;
+    Node() {
+        val = 0;
+        neighbors = vector<Node*>();
+    }
+    Node(int _val) {
+        val = _val;
+        neighbors = vector<Node*>();
+    }
+    Node(int _val, vector<Node*> _neighbors) {
+        val = _val;
+        neighbors = _neighbors;
+    }
+};
+*/
+
+class Solution {
+public:
+    unordered_map<Node*, Node*> mp;
+
+    // void DFS(Node* node, Node* clone_node) {
+    //     for(Node* n : node->neighbors) {
+    //         if(mp.find(n) == mp.end()) {
+    //             Node* clone = new Node(n->val);
+    //             mp[n] = clone;
+    //             clone_node->neighbors.push_back(clone);
+    //             DFS(n, clone);
+    //         }
+    //         else {
+    //             clone_node->neighbors.push_back(mp[n]);
+    //         }
+    //     }
+    // }
+
+    void BFS(queue<Node*> &que) {
+    
+        while(!que.empty()) {
+            Node* node = que.front();
+            que.pop();
+            Node* clone_node = mp[node];
+
+            for(Node* n: node->neighbors) {
+                if(mp.find(n) == mp.end()) {
+                    Node* clone = new Node(n->val);
+                    mp[n] = clone;
+                    clone_node->neighbors.push_back(clone);
+                    que.push(n);
+                }
+                else {
+                    clone_node->neighbors.push_back(mp[n]);
+                }
+            }
+        }
+    }
+
+    Node* cloneGraph(Node* node) {
+        if(node == NULL) {
+            return NULL;
+        }
+
+        mp.clear();
+
+        Node* clone_node = new Node(node->val);
+
+        mp[node] = clone_node;
+
+        queue<Node*> que;
+        que.push(node);
+
+        // DFS(node, clone_node);
+        BFS(que);
+
+        return clone_node;
+
+    }
+};
