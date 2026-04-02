79440c546e03760859b7563fcd81a48fc00b19b2
Kajal Kattige <35621828+kajalkattige30@users.noreply.github.com>
2026-04-02 17:46:54 +0530

    Add: rotting-fruit - submission-0

---
+++ Data Structures & Algorithms/rotting-fruit/submission-0.cpp
@@ -0,0 +1,57 @@
+class Solution {
+public:
+    typedef pair<int, int> P;
+
+    vector<vector<int>> directions{{1, 0}, {-1, 0}, {0, -1}, {0, 1}};
+
+    int orangesRotting(vector<vector<int>>& grid) {
+        int m = grid.size();
+        int n = grid[0].size();
+        int time = 0;
+        queue<P> que;
+        int freshCount = 0;
+
+        for(int i = 0; i < m; i++) {
+            for(int j = 0; j < n; j++) {
+                if(grid[i][j] == 2) {
+                    que.push({i, j});
+                }
+                else if(grid[i][j] == 1) {
+                    freshCount += 1;
+                }
+            }
+        }
+
+        if(freshCount == 0) {
+            return 0;
+        }
+
+        while(!que.empty()) {
+            int N = que.size();
+            while(N--) {
+                P curr = que.front();
+                que.pop();
+                int i = curr.first;
+                int j = curr.second;
+
+                for(vector<int> &dir: directions) {
+                    int new_i = i + dir[0];
+                    int new_j = j + dir[1];
+
+                    if(new_i >= 0 && new_i < m && new_j >= 0 && new_j < n && grid[new_i][new_j] == 1) {
+                        grid[new_i][new_j] = 2;
+                        que.push({new_i, new_j});
+                        freshCount--;
+                    }
+                }
+            }
+            time++;
+        }
+        if(freshCount == 0) {
+            return time-1;
+        }
+        else {
+            return -1;
+        }
+    }
+};
