3757ddb6cf3431f8d3e5bdb261556af193984b7e
Kajal Kattige <35621828+kajalkattige30@users.noreply.github.com>
2026-04-07 19:00:31 +0530

    Add: house-robber - submission-2

---
+++ Data Structures & Algorithms/house-robber/submission-2.cpp
@@ -0,0 +1,53 @@
+#include<cstring>
+class Solution {
+public:
+
+    /* Top Down
+    int t[101];
+
+    int solve(vector<int>& nums, int i) {
+        if(i >= nums.size()) {
+            return 0;
+        }
+
+        if(t[i] != -1) 
+            return t[i];
+
+        int steal = nums[i] + solve(nums, i+2);
+        int skip  = solve(nums, i+1);
+
+        return t[i] = max(steal, skip);
+    }
+    */
+    
+    /* Bottom-Up
+    int rob(vector<int>& nums) {
+        int n = nums.size();
+        int t[n+1];
+        t[0] = 0;
+        t[1] = nums[0];
+        for(int i = 2; i <= n; i++) {
+            int steal = nums[i-1] + t[i-2];
+            int skip  = t[i-1];
+
+            t[i] = max(steal, skip);
+        }
+        return t[n];
+    } */
+
+    //Bottom up with constant space
+    int rob(vector<int>& nums) {
+        int n = nums.size();
+        int prevPrev = 0;
+        int prev = nums[0];
+        for(int i = 2; i <= n; i++) {
+            int steal = nums[i-1] + prevPrev;
+            int skip  = prev;
+
+            int temp = max(steal, skip);
+            prevPrev = prev;
+            prev = temp;
+        }
+        return prev;
+    }
+};
