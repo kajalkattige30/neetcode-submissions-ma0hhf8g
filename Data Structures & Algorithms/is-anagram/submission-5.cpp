49d8a55689c93c17dc678edeaeb71ece5ebb191c
Kajal Kattige <35621828+kajalkattige30@users.noreply.github.com>
2026-06-04 15:22:29 +0530

    Add: is-anagram - submission-5

---
+++ Data Structures & Algorithms/is-anagram/submission-5.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        if(s.length() != t.length()) return false;
+        int s_freq[26] = {0};
+        int t_freq[26] = {0};
+
+        for(char &ch : s) {
+            s_freq[ch - 'a']++;
+        }
+        for(char &c : t) {
+            t_freq[c - 'a']++;
+        }
+
+        for(int i = 0; i < 26; i++) {
+            if(s_freq[i] != t_freq[i]) return false;
+        }
+
+        return true;
+    }
+};
